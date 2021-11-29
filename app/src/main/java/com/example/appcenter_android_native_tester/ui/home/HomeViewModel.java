f739979ed12eeec793492faa3da1c3753adb87c5
dragan1810 <d.novakovic1810@yahoo.com>
2021-11-29 13:05:20 +0100

    qqq

---
+++ app/src/main/java/com/example/appcenter_android_native_tester/ui/home/HomeViewModel.java
@@ -0,0 +1,19 @@
+package com.example.appcenter_android_native_tester.ui.home;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class HomeViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public HomeViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is home fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
